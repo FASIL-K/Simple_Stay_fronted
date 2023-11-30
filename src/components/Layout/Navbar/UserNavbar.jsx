b7cf0eaee4e47cd782194a6df33cd81906c92146
FASIL K <fasilmlm10@gmail.com>
2023-11-30 12:07:52 +0530

    added scroll effect

--- src/components/Layout/Navbar/UserNavbar.jsx
+++ src/components/Layout/Navbar/UserNavbar.jsx
@@ -1,32 +1,37 @@
-import React from "react";
-import {
-  Navbar,
-  MobileNav,
-  Typography,
-  Button,
-} from "@material-tailwind/react";
+import React, { useState, useEffect } from "react";
+import { Navbar, MobileNav, Typography, Button } from "@material-tailwind/react";
 import logo from "../../../assets/logo.svg";
 import avatar from "../../../assets/profileavatar.png";
 import { FiAlignJustify } from "react-icons/fi";
 import { FaRegHeart } from "react-icons/fa";
 import homeCover from "../../../assets/coverhome.png";
 import SearchBar from "../SearchBarHome/SearchBar";
+import './UserNavbar.css'
 
 export function StickyNavbar() {
-  const [openNav, setOpenNav] = React.useState(false);
+  const [scrolling, setScrolling] = useState(false);
 
-  React.useEffect(() => {
-    window.addEventListener(
-      "resize",
-      () => window.innerWidth >= 960 && setOpenNav(false)
-    );
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 420) {
+        setScrolling(true);
+      } else {
+        setScrolling(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
 
   const navList = (
-    <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6 ">
+    <ul className="mt-2 mb-4  flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6 ">
       <li>
-        <button>
-          <div className="h-8 w-16 bg-white rounded-full flex justify-start m">
+        <button className="">
+          <div className="h-8 w-16 bg-white rounded-full flex justify-start  mr-8">
             <FiAlignJustify className="text-black ml-2 mt-2" />
             <img className="h-6 mt-1 ml-1" src={avatar} alt="" />
           </div>
@@ -36,14 +41,14 @@ export function StickyNavbar() {
   );
 
   return (
-    <div className="relative">
-      <Navbar className="fixed top-0 z-10 h-16 max-w-full border-0 rounded-none p-2 bg-black bg-opacity-50 lg:px-8 lg:py-4 ">
-        <div className="flex items-center justify-between text-blue-gray-900">
+    <div className={`relative ${scrolling ? "scrolled" : ""}`}>
+      <Navbar  className={`fixed top-0 z-10 h-16 max-w-full border-0 rounded-none p-2 bg-black bg-opacity-50 lg:px-8 lg:py-4 transition-all duration-0   ${  scrolling ? "bg-blue-900 bg-opacity-100 w-[1200px] rounded-b-full " : "" } ${scrolling ? "transform translate-x-[-50%] left-1/2" : ""}`}>
+        <div className="flex items-center justify-between text-white">
           <div className="flex items-center">
             <img
               src={logo}
               alt="Logo"
-              className="h-11 md:h-12 lg:h-12 xl:h-12 ml-4 md:ml-8 lg:ml-12 xl:ml-16"
+              className="h-11 md:h-12 lg:h-12 xl:h-12 ml-4 md:ml-8 lg:ml-12 xl:ml-16 mb-4"
             />
           </div>
 
@@ -54,7 +59,7 @@ export function StickyNavbar() {
                 size="sm"
                 className="hidden sm:inline-block"
               >
-                <FaRegHeart className="h-6 w-9 mb-3" />
+                <FaRegHeart className="h-6 w-9 mb-3 text-white" />
               </Button>
             </div>
             <div className="mr-4">{navList}</div>
@@ -64,7 +69,6 @@ export function StickyNavbar() {
 
       <div className="relative">
         <div className="max-w-screen-2xl mx-auto">
-          {/* Adjust the max-width based on your design needs */}
           <img
             src={homeCover}
             alt="Cover"
@@ -83,7 +87,7 @@ export function StickyNavbar() {
         </div>
       </div>
 
-      <MobileNav open={openNav}>
+      <MobileNav >
         {navList}
         <div className="flex items-center gap-x-1">
           <Button fullWidth variant="text" size="sm">
