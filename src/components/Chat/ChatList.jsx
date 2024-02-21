862de4598ec5d5055f0ded4aac9279157c4ca47a
FASIL K <fasilmlm10@gmail.com>
2024-02-21 20:49:06 +0530

    added something

--- src/components/Chat/ChatList.jsx
+++ src/components/Chat/ChatList.jsx
@@ -6,7 +6,7 @@ import { GetChatList, getUserDetails } from "../../services/userApi";
 import { UserAxiosInstant } from "../../utils/axiosUtils";
 import profile from "../../assets/profileavatar.png";
 import { timeAgo } from "./TimeStamp";
-import { Websocket } from "../../Constants/Constants";
+import { Websocket } from "../../Constants/Constants.jsx";
 
 function UserChat() {
   const token = localStorage.getItem("token");
